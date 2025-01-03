439be17ffef0463999402645b235cc8bd4eda114
undefinedDen <tunevdenis123@gmail.com>
2025-01-03 17:31:26 +0200

    Take a method main and add exceptions by if-else

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,79 @@
 package app;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Main {
+    static int num;
+    static int count;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("-------------------------------------" +
+                "\n1.Square of a number");
+        System.out.print("Enter number for calculate it square: ");
+        num = scanner.nextInt();
+        int square = calculateSquareNum(num);
+        System.out.println("Square of number " + num + " = " + square);
+        System.out.println("---------------------------" +
+                "\n2.Calculating a volume of cylinder");
+        System.out.print("Enter radius: ");
+        double radius = scanner.nextDouble();
+        System.out.print("Enter high: ");
+        double high = scanner.nextDouble();
+        if(radius <= 0 || high <= 0){
+            System.out.println("Error! Radius and high can not be less or equals 0");
+        }else {
+            double volume = calculateCylinderVolume(radius, high);
+            System.out.println("The volume of cylinder with radius " + radius + " and high "
+                    + high + " equals " + volume);
+        }
+        System.out.println("----------------------------" +
+                "\n3.Calculating the sum of array value");
+        System.out.print("Enter size: ");
+        int size = scanner.nextInt();
+        if (size <= 0){
+            System.out.println("Error! Size of array can not be less or equals 0");
+        }else {
+            int[] array = new int[size];
+            count = 0;
+            System.out.println("Enter numbers: ");
+            do {
+                int arrayNumber = scanner.nextInt();
+                array[count] = arrayNumber;
+                count++;
+            } while (count < size);
+            int sum = calculateArraySum(array);
+            System.out.println("The sum value of array" + Arrays.toString(array) + " is " + sum);
+        }
+        scanner.nextLine();
+        System.out.println("--------------------------" +
+                "\n4.Turn the row over");
+        System.out.print("Enter row: ");
+        String row = scanner.nextLine();
+        String invertedRow = turnTheRowOver(row);
+        System.out.println("Inverted row is " + invertedRow);
+        System.out.println("----------------------------" +
+                "\n5.raise to a power ");
+        System.out.print("Enter number: ");
+        num = scanner.nextInt();
+        System.out.print("Enter power of a number: ");
+        int power = scanner.nextInt();
+        int result = raiseToAPower(num, power);
+        System.out.println("Result " + num + "^" + power + " = " + result);
+        System.out.println("-------------------------" +
+                "\n6.Repeat a text message");
+        System.out.print("Enter number of plays: ");
+        num = scanner.nextInt();
+        scanner.nextLine();
+        if(num <= 0 ){
+            System.out.println("Error! Number of plays can not be less or equals 0");
+        }else {
+            System.out.print("Enter a text for playing: ");
+            String text = scanner.nextLine();
+            repeatText(num, text);
+        }
     }
+
     private static int calculateSquareNum(int num) {
         return num * num;
     }
@@ -18,7 +88,6 @@ public class Main {
             sum += array[i];
         }
         return sum;
-        //TODO сделать запрос на длину масива а потом и на ввод||
     }
 
     private static String turnTheRowOver(String row) {
